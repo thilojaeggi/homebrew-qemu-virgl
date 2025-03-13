0a2cde2772bc008066751af9a5d1eb946dace1c5
startergo <7897244+startergo@users.noreply.github.com>
2025-03-12 20:39:08 -0400

    qemu-virgl: update to version 2025.03.13
    - Using QEMU commit ea35a5082a5fe81ce8fd184b0e163cd7b08b7ff7
    - Added patch from akihikodaki's macos branch
    - Configured for Metal backend using ANGLE
    - Added HVF acceleration support
    - Fixed style and formatting issues

--- Formula/qemu-virgl.rb
+++ Formula/qemu-virgl.rb
@@ -1,9 +1,9 @@
-# Formula created by startergo on 2025-03-12 21:18:25 UTC
+# Formula created by startergo on version 2025-03-13 00:35:44 UTC
 class QemuVirgl < Formula
   desc "Emulator for x86 and PowerPC"
   homepage "https://www.qemu.org/"
   url "https://github.com/qemu/qemu.git", using: :git, revision: "ea35a5082a5fe81ce8fd184b0e163cd7b08b7ff7"
-  version "2025.03.08"
+  version "2025.03.13"
   license "GPL-2.0-only"
 
   depends_on "libtool" => :build
@@ -27,7 +27,7 @@ class QemuVirgl < Formula
   depends_on "pixman"
   depends_on "snappy"
   depends_on "spice-protocol"
-  depends_on "vde"   
+  depends_on "vde"
 
   # Add tomli as a resource for Python build dependency
   resource "tomli" do
@@ -69,27 +69,32 @@ class QemuVirgl < Formula
       --prefix=#{prefix}
       --cc=#{ENV.cc}
       --host-cc=#{ENV.cc}
-      # Basic configuration
-      --disable-bsd-user
-      --disable-guest-agent
-      # Enable required features
       --enable-curses
       --enable-libssh
-      --enable-vde           
+      --enable-vde
+      --enable-virtfs
       # Explicitly disable GTK and GStreamer to prevent conflicts
       --disable-sdl
-      --disable-gtk      
+      --disable-gtk
       # Extra flags for includes and libraries
-      --extra-cflags=-DNCURSES_WIDECHAR=1
       --extra-cflags=-I#{Formula["startergo/homebrew-qemu-virgl/libangle"].opt_prefix}/include
       --extra-cflags=-I#{Formula["startergo/homebrew-qemu-virgl/libepoxy-angle"].opt_prefix}/include
       --extra-cflags=-I#{Formula["startergo/homebrew-qemu-virgl/virglrenderer"].opt_prefix}/include
-      --extra-cflags=-I#{Formula["spice-protocol"].opt_prefix}/include/spice-1
+      --extra-cflags=-march=armv8-a+crc+crypto
       --extra-ldflags=-L#{Formula["startergo/homebrew-qemu-virgl/libangle"].opt_prefix}/lib
       --extra-ldflags=-L#{Formula["startergo/homebrew-qemu-virgl/libepoxy-angle"].opt_prefix}/lib
       --extra-ldflags=-L#{Formula["startergo/homebrew-qemu-virgl/virglrenderer"].opt_prefix}/lib
-      --extra-ldflags=-L#{Formula["spice-protocol"].opt_prefix}/lib
     ]
+
+    # Create the wrapper script content
+    (bin/"qemu-wrapper").write <<~EOS
+      #!/bin/bash
+      export DYLD_FALLBACK_LIBRARY_PATH="#{Formula["startergo/homebrew-qemu-virgl/libangle"].opt_lib}:#{Formula["startergo/homebrew-qemu-virgl/libepoxy-angle"].opt_lib}:#{Formula["startergo/homebrew-qemu-virgl/virglrenderer"].opt_lib}:$DYLD_FALLBACK_LIBRARY_PATH"
+      export ANGLE_DEFAULT_PLATFORM=metal
+      exec "#{bin}/$1" "${@:2}"
+    EOS
+
+    chmod 0755, "#{bin}/qemu-wrapper"
 
     # Sharing Samba directories in QEMU requires the samba.org smbd which is
     # incompatible with the macOS-provided version. This will lead to
@@ -106,36 +111,27 @@ class QemuVirgl < Formula
 
   test do
     expected = "QEMU Project"
-    assert_match expected, shell_output("#{bin}/qemu-system-aarch64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-alpha --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-arm --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-cris --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-hppa --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-i386 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-m68k --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-microblaze --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-microblazeel --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mips --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mips64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mips64el --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-mipsel --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-nios2 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-or1k --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-ppc --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-ppc64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-riscv32 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-riscv64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-rx --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-s390x --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sh4 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sh4eb --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sparc --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-sparc64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-tricore --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-x86_64 --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-xtensa --version")
-    assert_match expected, shell_output("#{bin}/qemu-system-xtensaeb --version")
+    # Test system emulators
+    %w[
+      aarch64 alpha arm cris hppa i386 m68k microblaze microblazeel
+      mips mips64 mips64el mipsel nios2 or1k ppc ppc64 riscv32 riscv64
+      rx s390x sh4 sh4eb sparc sparc64 tricore x86_64 xtensa xtensaeb
+    ].each do |arch|
+      assert_match expected, shell_output("#{bin}/qemu-system-#{arch} --version")
+    end
+
+    # Test disk image handling
     resource("test-image").stage testpath
     assert_match "file format: raw", shell_output("#{bin}/qemu-img info FLOPPY.img")
+
+    # Test accelerator functionality
+    system "#{bin}/qemu-wrapper", "qemu-system-aarch64", "-accel", "help"
+
+    # Test HVF acceleration with minimum configuration
+    system "#{bin}/qemu-wrapper", "qemu-system-aarch64", \
+           "-machine", "virt,accel=hvf", \
+           "-cpu", "host", \
+           "-display", "none", \
+           "-m", "64"
   end
 end
