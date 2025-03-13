46997f1e07263af99e1aa402ca2f6fba62186371
startergo <7897244+startergo@users.noreply.github.com>
2025-03-13 01:04:02 -0400

    Updated timestamp to exactly match current time Updated wrapper script with simplified debugging commands Added register value examination Added string interpretation of registers Added frame disassembly Added function symbol examination Added stack memory inspection Maintained core functionality

--- Formula/qemu-virgl.rb
+++ Formula/qemu-virgl.rb
@@ -1,17 +1,19 @@
-# Formula created by startergo on version 2025-03-13 04:54:29 UTC
+# Formula created by startergo on version 2025-03-13 05:02:06 UTC
 class QemuVirgl < Formula
   desc "Emulator for x86 and PowerPC"
   homepage "https://www.qemu.org/"
-  url "https://github.com/qemu/qemu.git", using: :git, revision: "ea35a5082a5fe81ce8fd184b0e163cd7b08b7ff7" 
+  url "https://github.com/qemu/qemu.git", using: :git, revision: "ea35a5082a5fe81ce8fd184b0e163cd7b08b7ff7"
   version "2025.03.13"
   license "GPL-2.0-only"
 
+  # Build dependencies
   depends_on "libtool" => :build
-  depends_on "meson" => :build
+  depends_on "meson" => :build 
   depends_on "ninja" => :build
   depends_on "pkg-config" => :build
   depends_on "python@3.13" => :build
 
+  # Runtime dependencies
   depends_on "coreutils"
   depends_on "glib"
   depends_on "gnutls"
@@ -143,15 +145,14 @@ class QemuVirgl < Formula
            -o "process handle -p true -s true SIGSEGV" \\
            -o "b get_opt_value" \\
            -o "run" \\
-           -o "frame info" \\
-           -o "frame variable -A" \\
-           -o "memory read -f x -s 8 \$x0 \$x0+256" \\
-           -o "register read x0 x1 x2 x26" \\
-           -o "disassemble -b -p" \\
-           -o "si" \\
-           -o "register read x0 x1 x2 x26 sp pc" \\
-           -o "memory read -f x -s 8 \$sp \$sp+64" \\
-           -o "bt all" \\
+           -o "register read --all" \\
+           -o "p/x \$x0" \\
+           -o "p/x \$x26" \\
+           -o "p (char*)\$x26" \\
+           -o "disassemble --frame" \\
+           -o "thread backtrace all" \\
+           -o "expression -- get_opt_value" \\
+           -o "memory read --size 8 --format x --count 32 \$sp" \\
            -o "quit" \\
            -- "$QEMU_CMD" "$@" 2>&1 | tee -a "$LOG_FILE"
     EOS
